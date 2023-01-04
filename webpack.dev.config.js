aea7eb1176a352caf5b7fe524aefe75d87489488
ibon eskudero <ibon.eskudero@tecnalia.com>
2023-01-04 10:10:17 +0100

    add webpack base for a better build control

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,114 +1,44 @@
-/* eslint-disable no-undef */
-
-var path = require( 'path' );
+const path = require( 'path' );
 const express = require( 'express' );
- 
+const { merge } = require( 'webpack-merge' );
+const webpackConfigBase = require( './webpack.base.config.js' );
+
 const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
 const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
-const CaseSensitivePathsPlugin = require( 'case-sensitive-paths-webpack-plugin' );
   
-module.exports = ( env ) => {
- 
-	 return {
-		 entry: {
-			 main: './example/index.js',
-			 css: './example/index.css',
-		 },
-		 output: {
-			 path: path.resolve( __dirname, './dist' ),
-			 filename: '[name].js',
-			 library: {
-				 name: 'library',
-				 type: 'assign-properties'
-			 }
-		 },
-		 resolve: {
-			 fallback: {
-				 crypto: require.resolve( 'crypto-browserify' ),
-				 stream: require.resolve( 'stream-browserify' ),
-				 buffer: require.resolve( 'buffer' )
-			 }
-		 },
-		 target: 'web',
-		 module: {
-			 rules: [
-				 {
-					 test: /\.html$/,
-					 use: {
-						 loader: 'html-loader',
-						 options: {
-							 minimize: false
-						 }
-					 }
-				 },		
-				 { /* IMPORT FOR CSS MODULES. CSS FILES THAT ARE IMPORTED FROM JS */
-					 test: /\.css$/,
-					 use: [ 'to-string-loader',{
-							 loader: 'css-loader',
-							 options: {
-								 importLoaders: 1,
-								 modules: 'global',
-								 sourceMap: true
-							 }
-						 }
-					 ],
-					 include: /\.module\.css$/,
-				   },
-				 { /* IMPORT FOR CSS FILES. CSS FILES THAT ARE ADDED FROM WEBPACK & CSS FILES */
-					 test: /\.css$/,
-					 use: [ {
-							 loader: MiniCssExtractPlugin.loader,
-							 options: {}
-						 }, {
-							 loader: 'css-loader',
-							 options: { sourceMap: true }
-						 }
-					 ],
-					 exclude: /\.module\.css$/,
-				 },
-				 { 
-					//get url when jpg
-					test: /\.jpe?g$/, 
-					type: 'asset/inline'
-				 },
-				 {
-					 test: /\.(svg|png|gif|ico|eot|ttf|woff|woff2?)(\?v=\d+\.\d+\.\d+)?$/i,
-					 type: 'asset/resource'
-				 }
-			 ]
-		 },
-		 plugins: [
-			 new HtmlWebPackPlugin( {
-				 template: './example/index.html',
-				 filename: './index.html',
-				 chunks: [ 'main', 'css' ]
-			 } ),
-			 new MiniCssExtractPlugin( {
-				 filename: '[name].css',
-				 chunkFilename: '[id].css'
-			 } ),
-			 new CaseSensitivePathsPlugin()
-		 ],
-		 stats: {
-			 colors: true
-		 },
-		 devServer: {
-			static: './dist',
-			hot: false,
-			//MAPPING local wwwdata for webpack-dev-server
-			setupMiddlewares: ( middlewares, devServer ) => {
+const webpackConfigDev = {
+	entry: {
+		main: './example/index.js',
+		css: './example/index.css',
+	},
+	plugins: [
+		new HtmlWebPackPlugin( {
+			template: './example/index.html',
+			filename: './index.html',
+			chunks: [ 'main', 'css' ]
+		} ),
+		new MiniCssExtractPlugin( {
+			filename: '[name].css',
+			chunkFilename: '[id].css'
+		} )
+	],
+	devServer: {
+		static: './dist',
+		hot: false,
+		setupMiddlewares: ( middlewares, devServer ) => {
 
-				devServer.app.use( '/fonts/', express.static( path.resolve( __dirname, './example/fonts' ) ) );	
-				devServer.app.use( '/resources/', express.static( path.resolve( __dirname, './resources' ) ) );				
+			devServer.app.use( '/fonts/', express.static( path.resolve( __dirname, './example/fonts' ) ) );	
+			devServer.app.use( '/resources/', express.static( path.resolve( __dirname, './resources' ) ) );				
 
-				return middlewares;
-			},
-			devMiddleware: {
-				writeToDisk: true
-			}
+			return middlewares;
 		},
-		 devtool: 'inline-source-map',	//inline-source-map necessary instead of source-map for chrome dev tools workspace to work
-		 mode: 'development'
-	 };
+		devMiddleware: {
+			writeToDisk: true
+		}
+	},
+	devtool: 'inline-source-map',	//inline-source-map necessary instead of source-map for chrome dev tools workspace to work
+	mode: 'development'
 };
+
+module.exports = merge( webpackConfigBase, webpackConfigDev );
  
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,15 +1,11 @@
 const { merge } = require( 'webpack-merge' );
 const webpackConfigBase = require( './webpack.base.config.js' );
 
-const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
 const TerserPlugin = require( 'terser-webpack-plugin' );
-const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
-const CopyPlugin = require( 'copy-webpack-plugin' );
 
 const webpackConfigDev = {
 	entry: {
-		main: './example/index.js',
-		css: './example/index.css',
+		main: './main.js',
 	},
 	optimization: {
 		minimize: true,
@@ -37,25 +33,9 @@ const webpackConfigDev = {
 			}
 		}
 	},
-	plugins: [
-		new HtmlWebPackPlugin( {
-			template: './example/index.html',
-			filename: './index.html',
-			chunks: [ 'main', 'css' ]
-		} ),
-		new MiniCssExtractPlugin( {
-			filename: '[name].css',
-			chunkFilename: '[id].css'
-		} ),
-		new CopyPlugin( {
-			patterns: [
-				{ from: 'example/fonts', to: 'fonts' },
-			],
-		} )
-	],
 	watch: false,
 	devtool: false,
 	mode: 'production'
 };
- 
+
 module.exports = merge( webpackConfigBase, webpackConfigDev );
