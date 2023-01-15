4daf65d18788dcda83d9a52771bf9b712c20cdaa
ibon eskudero <ibon.eskudero@tecnalia.com>
2023-01-15 19:17:43 +0100

    add plain example, without merge. Useful for debugging

---
+++ example/plain/index.js
@@ -0,0 +1,27 @@
+import { DXFViewer } from '../../src/dxfViewer.js';
+import { Boilerplate } from '../boilerplate.js';
+
+//global variables
+const font = '../fonts/helvetiker_regular.typeface.json';
+
+//init html
+let html = new Boilerplate();
+html.onLoad = async ( file ) => {
+	html.three.clear();
+
+	let dxf = await new DXFViewer().getFromFile( file, font );
+	
+	if( dxf ) {
+		//add dxf
+		html.three.addDXF( dxf );
+	}
+};
+
+html.init();
+  
+
+
+
+
+
+
