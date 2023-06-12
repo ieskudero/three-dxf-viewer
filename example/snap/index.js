18e8612642fd21a28d65a3f4d25e74976de24281
ibon eskudero <ibon.eskudero@tecnalia.com>
2023-06-12 15:28:04 +0200

    add snap example

---
+++ example/snap/index.js
@@ -0,0 +1,32 @@
+import { DXFViewer } from '../../src/dxfViewer.js';
+import { SnapsHelper } from '../../src/utils/snapsHelper.js';
+import { Boilerplate } from '../boilerplate.js';
+
+//global variables
+const font = 'fonts/helvetiker_regular.typeface.json';
+let snaps;
+
+//init html
+let html = new Boilerplate();
+html.onLoad = async ( file ) => {
+	html.three.clear();
+
+	let dxf = await new DXFViewer().getFromFile( file, font );
+	if( dxf ) {
+
+		if( snaps ) snaps.clear();
+		snaps = new SnapsHelper( dxf, html.three.renderer, html.three.scene, html.three.camera, html.three.controls );
+
+		//add dxf
+		html.three.addDXF( dxf );
+	}
+};
+
+html.init();
+  
+
+
+
+
+
+
