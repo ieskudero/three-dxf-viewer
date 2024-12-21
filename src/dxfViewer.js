f41d1f1aafbeb279ad17101843dd2ba4cac976a8
ibon eskudero <ibon.eskudero@tecnalia.com>
2024-12-21 12:10:10 +0100

    change unit property to number. This way it can be compared with UNITS enumerator directly

--- src/dxfViewer.js
+++ src/dxfViewer.js
@@ -14,7 +14,6 @@ import * as Helper from 'dxf/src/Helper';
 import { Properties } from './entities/baseEntity/properties';
 import { LayerHelper } from './entities/baseEntity/layerHelper.js';
 import { ColorHelper } from './entities/baseEntity/colorHelper.js';
-import { UNITS } from './entities/baseEntity/eUnit.js';
 
 /**
  * @class DXFViewer
@@ -86,8 +85,7 @@ export class DXFViewer {
 			this.layers = data.tables.layers;
 
 			//export global unit
-			const toEnum = v => Object.keys( UNITS ).find( key => UNITS[key] === v );
-			this.unit = data.header ? toEnum( data.header.insUnits ) : 0;
+			this.unit = data.header ? data.header.insUnits : 0;
 
 			//return draw
 			resolve( await this._drawDXF( data ) );
